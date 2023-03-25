9e6e9dc458e0a7afaf1df0a14df985727629b390
iXGEN <106850727+iXGEN@users.noreply.github.com>
2023-03-25 15:34:08 -0300

    QR Code URL

--- web/frontend/components/QRCodeForm.jsx
+++ web/frontend/components/QRCodeForm.jsx
@@ -19,16 +19,73 @@ import {
 import {
   ContextualSaveBar,
   ResourcePicker,
+  useAppBridge,
   UseAppBridge,
+  useAuthenticatedFetch,
   useNavigate,
 } from "@shopify/app-bridge-react";
 import { ImageMajor, AlertMinor } from "@shopify/polaris-icons";
-import { useForm, useField, noEmptyString } from "@shopify/react-form";
+import {
+  useForm,
+  useField,
+  noEmptyString,
+  notEmptyString,
+} from "@shopify/react-form";
 
 const NO_DISCOUNT_OPTION = { label: "No discount", value: "" };
 
 const DISCOUNT_CODES = {};
 
 export default QRCodeForm = ({ QRCode: InitialQRCode }) => {
+  const [QRCode, setQRCode] = useState(InitialQRCode);
+  const [shownResourcePicker, setShownResourcePicker] = useState(false);
+  const [selectedProduct, setSelectedProduct] = useState(QRCode?.product);
+  const navigate = useNavigate();
+  const appBridge = useAppBridge();
+  const fetch = useAuthenticatedFetch();
+  const deletedProduct = QRCode?.product?.title === "Deleted product";
+
+  const onSubmit = (body) => console.log("submit", body);
+
+  const {
+    fields: {
+      title,
+      productId,
+      variantId,
+      handle,
+      discountId,
+      discountCode,
+      destination,
+    },
+    dirty,
+    reset,
+    submitting,
+    submit,
+    makeClean,
+  } = useForm({
+    fields: {
+      title: useField({
+        value: QRCode?.title || "",
+        validates: [notEmptyString("Please name your QR code")],
+      }),
+      productId: useField({
+        value: deletedProduct ? "Deleted product" : QRCode?.product?.id || "",
+        validates: [notEmptyString("Please select a product")],
+      }),
+      variantId: useField(QRCode?.variantId || ""),
+      handle: useField(QRCode?.handle || ""),
+      destination: useField(
+        QRCode?.destination ? [QRCode.destination] : ["product"]
+      ),
+      discountId: useField(QRCode?.discountId || NO_DISCOUNT_OPTION.value),
+      discountCode: useField(QRCode?.discountCode || ""),
+    },
+    onSubmit,
+  });
+
+  const QRCodeURL = QRCode
+    ? new URL(`/qrcodes/${QRCode.id}/image`, location.toString()).toString()
+    : null;
+
   return <div>QR Codes</div>;
 };
